f8a2b38e8c26304f00fb533eeb454485bb2d4b73
Alex Hokanson <ingshtrom.developer@gmail.com>
2018-02-07 09:17:02 -0500

    contentReducer tests done

--- react/src/modules/__tests__/content.spec.js
+++ react/src/modules/__tests__/content.spec.js
@@ -56,3 +56,62 @@ test('GET_CONTENT_FAILURE action sets isLoading=false and sets the errorMessage 
   expect(contentReducer(startState, action)).toMatchObject(endState);
 });
 
+test('GET_MEDIA_FAILURE action sets isLoading=false and sets the errorMessage according to the action', () => {
+  const startState = fromJS({
+    data: {},
+    media: {},
+    errorMessage: '',
+    isLoading: true
+  });
+  const endState = fromJS({
+    data: {},
+    media: {},
+    errorMessage: '!boobaz!',
+    isLoading: false
+  });
+  const action = { type: 'GET_MEDIA_FAILURE', errorMessage: '!boobaz!' };
+
+  expect(contentReducer(startState, action)).toMatchObject(endState);
+});
+
+test('GET_CONTENT_SUCCESS action sets isLoading=false and sets the data according to the action', () => {
+  const startState = fromJS({
+    data: {},
+    media: {},
+    errorMessage: '',
+    isLoading: true
+  });
+  const endState = fromJS({
+    data: {
+      does_not: 'matter what this is, just that it is set'
+    },
+    media: {},
+    errorMessage: '',
+    isLoading: false
+  });
+  const action = { type: 'GET_CONTENT_SUCCESS', data: { does_not: 'matter what this is, just that it is set' }};
+
+  expect(contentReducer(startState, action)).toMatchObject(endState);
+});
+
+test('GET_MEDIA_SUCCESS action sets isLoading=false and sets the media according to the action', () => {
+  const startState = fromJS({
+    data: {},
+    media: {},
+    errorMessage: '',
+    isLoading: true
+  });
+  const endState = fromJS({
+    data: {
+    },
+    media: {
+      does_not: 'matter what this is, just that it is set'
+    },
+    errorMessage: '',
+    isLoading: false
+  });
+  const action = { type: 'GET_MEDIA_SUCCESS', data: { does_not: 'matter what this is, just that it is set' }};
+
+  expect(contentReducer(startState, action)).toMatchObject(endState);
+});
+
