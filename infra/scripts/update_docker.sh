9d0e158283de1c6b5d3df2e837416d55da457cf4
Alex Hokanson <ingshtrom.developer@gmail.com>
2018-02-27 09:53:44 -0500

    docker stack with local build

--- infra/scripts/update_docker.sh
+++ infra/scripts/update_docker.sh
@@ -5,6 +5,10 @@ set -e
 sudo mkdir -p /data/mysql
 sudo chmod +rwx /data/mysql
 
+cd /opt/playlister/node
+
+sudo docker build . --rm --no-cache -t ingshtrom/playlister:local
+
 cd /opt/playlister
 
 sudo docker stack deploy -c /opt/playlister/docker-stack-production.yml --prune prd
