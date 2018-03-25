1dfd7300b9e016c146546d92a556c374d498133f
ingshtrom <alex.hokanson@docker.com>
2018-03-25 16:35:28 -0400

    fix delete and rename behavior
    * before, it wouldn't let you create a new container with the same name
    as a deleted container in the same folder. This was because of a
    uniqueness constraint on the Container.fullPath column in the database.
    This has been removed in favor of a compound constraint of uniqueness on
    a combination of the fields ['fullPath', 'deletedAt']

---
+++ node/migrations/20180325201741-unique-container-name.js
@@ -0,0 +1,26 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addConstraint('Containers', ['fullPath', 'deletedAt'], {
+      type: 'unique',
+      name: 'unique_full_path_and_exist'
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.removeConstraint('Containers', 'unique_full_path_and_exist');
+  }
+};
